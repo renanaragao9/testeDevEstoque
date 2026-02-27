d99437a228cc3a64ff97cfc1f8d666f7f3fe4503
renanaragao9 <renanaragao9@github.com>
2026-02-27 14:27:24 -0300

    [FTR]

--- frontend/src/components/dashboard/StatsWidget.vue
+++ frontend/src/components/dashboard/StatsWidget.vue
@@ -21,12 +21,14 @@ const formatCurrency = (value: number): string => {
                         {{ dashboardStore.loading ? '...' : dashboardStore.totalSales }}
                     </div>
                 </div>
+
                 <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-shopping-cart text-blue-500 !text-xl"></i>
                 </div>
             </div>
+
             <span class="text-primary font-medium">{{ dashboardStore.totalQuantitySold }} </span>
-            <span class="text-muted-color">itens vendidos</span>
+            <span class="text-muted-color pl-1">itens vendidos</span>
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
@@ -38,12 +40,13 @@ const formatCurrency = (value: number): string => {
                         {{ dashboardStore.loading ? '...' : formatCurrency(dashboardStore.totalSalesAmount) }}
                     </div>
                 </div>
+
                 <div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-dollar text-orange-500 !text-xl"></i>
                 </div>
             </div>
             <span class="text-primary font-medium">Total </span>
-            <span class="text-muted-color">em vendas</span>
+            <span class="text-muted-color pl-1">em vendas</span>
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
@@ -55,12 +58,13 @@ const formatCurrency = (value: number): string => {
                         {{ dashboardStore.loading ? '...' : dashboardStore.totalCustomers }}
                     </div>
                 </div>
+
                 <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-users text-cyan-500 !text-xl"></i>
                 </div>
             </div>
             <span class="text-primary font-medium">Total </span>
-            <span class="text-muted-color">cadastrados</span>
+            <span class="text-muted-color pl-1">cadastrados</span>
         </div>
     </div>
     <div class="col-span-12 lg:col-span-6 xl:col-span-3">
@@ -72,12 +76,13 @@ const formatCurrency = (value: number): string => {
                         {{ dashboardStore.loading ? '...' : dashboardStore.totalProducts }}
                     </div>
                 </div>
+
                 <div class="flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-box text-purple-500 !text-xl"></i>
                 </div>
             </div>
             <span class="text-primary font-medium">{{ dashboardStore.totalPurchases }} </span>
-            <span class="text-muted-color">compras realizadas</span>
+            <span class="text-muted-color pl-1">compras realizadas</span>
         </div>
     </div>
 </template>
